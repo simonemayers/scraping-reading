dee84ba4d79048c82ffba9a121214cdc7f53fd00
simonemayers <simone21295@gmail.com>
2020-12-02 15:15:02 -0500

    commit tree b9f0cd4facce2776760a37c42f1de2870b72f18f

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,14 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+
+puts doc.css(".headline-26OIBN").text 
+courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+courses.each do |course|
+    puts course.text.strip
+end
+
+p doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")[0].name
+p doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")[0].attributes 
+
